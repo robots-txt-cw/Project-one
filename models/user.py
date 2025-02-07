c65feb2823337203d2d9e4dd5a69ae411020fa24
Your Name <you@example.com>
2025-02-06 22:35:53 -0700

    Partly added "split" compatability to User Cleaned up _getPlayOptions() for readability

--- models/user.py
+++ models/user.py
@@ -1,25 +1,43 @@
 from player import Player
-
+from card import Card
+from typing import *
 
 class User(Player):
+
+    def __init__(self):
+        super().__init__()
+        self.splitHand: Optional[List[Card]] = None
+
+        
 
     def printHand(self):
         for i in self.hand:
             i.print()
         print(f"Currently {self.handValue}")
 
-    def _getPlayOptions(self, turns: int) -> list[str]:
+    def _getPlayOptions(self, hand: List[Card], turns: int) -> List[str]:
         '''Private method.
         Returns the options the player has available to them from a given amount of previous turns.
         Args:
             turns: The amount of previous turns the player has played. Used to determine if "Double Down" and "Split" is available.
+        Return:
+            List of available options
         '''
         options = ['s', 'h']
         # Double Down
         if turns == 0:
             options.append('d')
         # Split
-        if turns == 0 and self.hand[0].cardValue == self.hand[1].cardValue:
+        def checksplit():
+            available = True
+            if turns > 0 and self.hand:
+                available = False
+            if hand[0].cardValue != hand[1].cardValue:
+                available = False
+            
+            return available
+            
+        if checksplit():
             options.append('s')
         return options
 
